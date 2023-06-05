20d6eca619474479a567facf04f0b79072131acc
Lindjunne Montenegro II <lgbmontenegroii@outlook.ph>
2023-06-05 19:18:54 +0800

    chore_sinonimity: 20230605-v1 Update - Move dependencies to devDependencies - Modify default tailwind configuration
    Signed-off-by: Lindjunne Montenegro II <lgbmontenegroii@outlook.ph>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,10 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+/**
+ * Flowbite Configuration
+ * https://flowbite-svelte.com/docs/pages/introduction#Configuration
+ */
+
 /** @type {import('tailwindcss').Config} */
 const config = {
     content: [
@@ -5,7 +12,15 @@ const config = {
         './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                // flowbite-svelte
+                primary: { 50: '#ebf5ff', 100: '#fff1ee', 200: '#ffe4de', 300: '#ffd5cc', 400: '#ffbcad', 500: '#fe795d', 600: '#ef562f', 700: '#eb4f27', 800: '#d3330a', 900: '#d3330a' },
+            },
+        },
+        fontFamily: {
+            sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        },
     },
     plugins: [
         import( 'flowbite/plugin' ),
